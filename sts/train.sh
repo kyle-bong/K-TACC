f2d2066f2783c7cb18c3ffc2c96e07c2cb8631b4
root <wdg369@gmail.com>
2023-02-27 02:45:07 +0000

    backup

---
+++ sts/train.sh
@@ -0,0 +1,14 @@
+# # base
+# python train.py --config base_config
+
+# # RMR
+# python train.py --config random_masking_replacement_config
+
+# # RMI
+# python train.py --config random_masking_insertion_config
+
+# adverb
+# python train.py --config adverb_config
+
+# EDA
+python
