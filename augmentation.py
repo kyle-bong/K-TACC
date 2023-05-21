2f18c00974dc6d53d56f6a073c3b1eaf418e9f3b
bongseok <wdg369@gmail.com>
2023-05-22 00:24:12 +0900

    Add: tqdm

--- augmentation.py
+++ augmentation.py
@@ -2,6 +2,8 @@
 import pandas as pd
 from BERT_augmentation import BERT_Augmentation
 from adverb_augmentation import AdverbAugmentation
+from tqdm import tqdm
+tqdm.pandas()
 
 BERT_aug = BERT_Augmentation()
 random_masking_replacement = BERT_aug.random_masking_replacement
@@ -13,8 +15,8 @@ adverb_gloss_replacement = adverb_aug.adverb_gloss_replacement
 orig_train = pd.read_json('sts/datasets/klue-sts-v1.1_train.json')
 sen1_random_masking_replacement_train = orig_train.copy()
 sen2_random_masking_replacement_train = orig_train.copy()
-sen1_random_masking_replacement_train['sentence1'] = sen1_random_masking_replacement_train['sentence1'].apply(lambda x: random_masking_replacement(x, span=1) if len(x.split()) > 1 else x)
-sen2_random_masking_replacement_train['sentence2'] = sen2_random_masking_replacement_train['sentence2'].apply(lambda x: random_masking_replacement(x, span=1) if len(x.split()) > 1 else x)
+sen1_random_masking_replacement_train['sentence1'] = sen1_random_masking_replacement_train['sentence1'].progress_apply(lambda x: random_masking_replacement(x, span=1) if len(x.split()) > 1 else x)
+sen2_random_masking_replacement_train['sentence2'] = sen2_random_masking_replacement_train['sentence2'].progress_apply(lambda x: random_masking_replacement(x, span=1) if len(x.split()) > 1 else x)
 random_masking_replacement_augset = pd.concat([orig_train, sen1_random_masking_replacement_train, sen2_random_masking_replacement_train])
 random_masking_replacement_augset.drop_duplicates(['sentence1', 'sentence2'], inplace=True)
 print(len(random_masking_replacement_augset))
@@ -24,8 +26,8 @@ random_masking_replacement_augset.reset_index().to_json('sts/datasets/klue-sts-v
 
 sen1_random_masking_insertion_train = orig_train.copy()
 sen2_random_masking_insertion_train = orig_train.copy()
-sen1_random_masking_insertion_train['sentence1'] = sen1_random_masking_insertion_train['sentence1'].apply(lambda x: random_masking_insertion(x))
-sen2_random_masking_insertion_train['sentence2'] = sen2_random_masking_insertion_train['sentence2'].apply(lambda x: random_masking_insertion(x))
+sen1_random_masking_insertion_train['sentence1'] = sen1_random_masking_insertion_train['sentence1'].progress_apply(lambda x: random_masking_insertion(x))
+sen2_random_masking_insertion_train['sentence2'] = sen2_random_masking_insertion_train['sentence2'].progress_apply(lambda x: random_masking_insertion(x))
 random_masking_insertion_augset = pd.concat([orig_train, sen1_random_masking_insertion_train, sen2_random_masking_insertion_train])
 random_masking_insertion_augset.drop_duplicates(['sentence1', 'sentence2'], inplace=True)
 print(len(random_masking_insertion_augset))
@@ -33,8 +35,8 @@ random_masking_insertion_augset.reset_index().to_json('sts/datasets/klue-sts-v1.
 
 sen1_adverb_train = orig_train.copy()
 sen2_adverb_train = orig_train.copy()
-sen1_adverb_train['sentence1'] = sen1_adverb_train['sentence1'].apply(lambda x: adverb_gloss_replacement(x))
-sen2_adverb_train['sentence2'] = sen2_adverb_train['sentence2'].apply(lambda x: adverb_gloss_replacement(x))
+sen1_adverb_train['sentence1'] = sen1_adverb_train['sentence1'].progress_apply(lambda x: adverb_gloss_replacement(x))
+sen2_adverb_train['sentence2'] = sen2_adverb_train['sentence2'].progress_apply(lambda x: adverb_gloss_replacement(x))
 adverb_augset = pd.concat([orig_train, sen1_adverb_train, sen2_adverb_train])
 adverb_augset.drop_duplicates(['sentence1', 'sentence2'], inplace=True)
 print(len(adverb_augset))
