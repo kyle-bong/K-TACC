985247f5391e1856f930c52fc8d6f1e9448b902d
root <wdg369@gmail.com>
2023-05-31 03:34:36 +0000

    Fix: train.sh

--- sts/train.sh
+++ sts/train.sh
@@ -1,14 +1,23 @@
-# # base
-# python train.py --config base_config
+# base
+python train.py --config base_config
 
-# # RMR
-# python train.py --config random_masking_replacement_config
+# RMR
+python train.py --config random_masking_replacement1_config
 
-# # RMI
-# python train.py --config random_masking_insertion_config
+python train.py --config random_masking_replacement2_config
+
+# RMI
+python train.py --config random_masking_insertion1_config
+python train.py --config random_masking_insertion2_config
 
 # adverb
-# python train.py --config adverb_config
+python train.py --config adverb_config
+
+# adea
+python train.py --config aeda_config
 
 # EDA
-python
+python train.py --config random_deletion_config
+python train.py --config random_insertion_config
+python train.py --config random_swap_config
+python train.py --config random_synonym_replacement_config
